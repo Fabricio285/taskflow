776176c502190a61af3e3dcf4571554758bab81c
CAF_285 <aldofabriciocruz@gmail.com>
2026-01-28 13:31:36 -0300

    Add .tsx extension to App import
    Updated import statement for App component to include .tsx extension.

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// Añadimos la extensión .tsx para ayudar al navegador en entornos estáticos
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
